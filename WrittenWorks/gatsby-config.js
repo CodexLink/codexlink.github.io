1b4bff202fe6e4b002916647da98be7f75ecfd09
Janrey Licas <janjan.licas@gmail.com>
2020-03-31 17:59:19 +0800

    Deployment | Changed Config to support USERNAME.github.io

--- WrittenWorks/gatsby-config.js
+++ WrittenWorks/gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-  pathPrefix: "/SignedWrittenWorks",
+    `gatsby-plugin-offline`,
+  ]
 }
