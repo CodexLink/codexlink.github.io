354f7a9421309271bc7d7c9b2469fbc9e3badaed
Janrey Licas <janjan.licas@gmail.com>
2020-04-05 01:17:13 +0800

    Theme Implementation | Added 80% Required Property

---
+++ WrittenWorks/src/styles/theme.js
@@ -0,0 +1,26 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { amber, orange, red } from '@material-ui/core/colors';
+
+let theme = createMuiTheme({
+    palette: {
+        primary: amber,
+        secondary: red,
+    },
+    status: {
+        danger: orange,
+    },
+    typography: {
+        fontFamily: [
+            'Roboto',
+            '"Roboto Mono"',
+        ].join(',')
+    },
+    title: {
+        flexGrow: 1,
+    },
+});
+
+export default theme = responsiveFontSizes(theme);
+
+// TODO: Provide Font Variants for Specific Use Case.
+// @o | https://material-ui.com/customization/typography/
