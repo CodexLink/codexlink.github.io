7766a079089b8f420cf61ef37c583b649a31cd80
Janrey Licas <janjan.licas@gmail.com>
2020-04-05 15:26:15 +0800

    HeaderJS | Completion at 30%.

--- WrittenWorks/src/components/header.jsx
+++ WrittenWorks/src/components/header.jsx
@@ -1,41 +1,53 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import { AppBar, Box, Button, IconButton, Toolbar, Tooltip, Typography, useMediaQuery } from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu";
-import { fade, makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import FlareIcon from '@material-ui/icons/Flare';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import {makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import CustomMUI from "../styles/theme";
 
 // TODO: ADD FUNCTIONALITY AND CLASS ES6 CODE STYLE.
 // @PROMISE - ATTEMPT TO MAKE ONE STYLESHEET IN ONE PLACE.
+// @PROMISE - ADD RESPONSIVENESS BY HIDING TITLE ON LOW-RES DEVICES.
 
-const HeaderStyleSheet = makeStyles((InjectionElem) => ({
+const HeaderStyleSheet = makeStyles((InjectElem) => ({
 	root: {
 		flexGrow: 1,
 	},
 	menuButton: {
-		marginRight: InjectionElem.spacing(2),
+		marginRight: InjectElem.spacing(2),
+	},
+	buttonModifiers: {
+		marginRight: InjectElem.spacing(2),
 	},
 	appTitle: {
 		flexGrow: 1,
 	},
+
 }));
 
+// ! Deconstructing this function is not viable. Though, not very complex to do so.
 export default ({ prop, children }) => {
-	console.log(prop);
-	console.log("prop Done");
-	console.log(children);
-	console.log("children Done");
-	const headerStyles = HeaderStyleSheet();
 	const headerTitleQuery = useStaticQuery(graphql`
 	query {
 		site {
 			siteMetadata {
 				title
+				title_secondary
 				author
 			}
 		}
 	}
 	`);
+
+	let headerDynamicTitle = ((useMediaQuery("(min-width:670px)")) ? headerTitleQuery.site.siteMetadata.title : headerTitleQuery.site.siteMetadata.title_secondary);
+	let headerDynamicAuthor = (useMediaQuery("(min-width:445px)") ? `| By ${headerTitleQuery.site.siteMetadata.author}` : "");
+
+	const headerStyles = HeaderStyleSheet();
 
 	return (
 		<ThemeProvider theme={CustomMUI}>
@@ -46,9 +58,23 @@ export default ({ prop, children }) => {
 							<MenuIcon />
 						</IconButton>
 						<Typography variant="h6" className={headerStyles.appTitle}>
-							{`${headerTitleQuery.site.siteMetadata.title} | By ${headerTitleQuery.site.siteMetadata.author}`}
+							{`${headerDynamicTitle} ${headerDynamicAuthor}`}
 						</Typography>
-						<Button color="inherit">Login</Button>
+						<Tooltip title="Toggle Dark / Light Mode" arrow>
+							<IconButton edge="start" color="inherit" className={headerStyles.buttonModifiers} aria-label="theme-toggler">
+								<FlareIcon />
+							</IconButton>
+						</Tooltip>
+						<Tooltip title="Open Github Repo Of This Web App" arrow>
+							<IconButton edge="start" color="inherit" target="__target" rel="noopener noreferrer"href="https://github.com/CodexLink/codexlink.github.io" className={headerStyles.buttonModifiers} aria-label="repo-redirector">
+								<GitHubIcon />
+							</IconButton>
+						</Tooltip>
+						<Tooltip title="More..." arrow>
+							<IconButton edge="start" color="inherit" aria-label="web-app-info-spreader">
+								<MoreVertIcon />
+							</IconButton>
+						</Tooltip>
 					</Toolbar>
 				</AppBar>
 			</Box>
