fb5f47065d3ded5448471f36e5df2fa026a66c07
Janrey Licas <janjan.licas@gmail.com>
2020-04-06 16:07:36 +0800

    Sidebar | Prepped Components. (Non-Rendering)

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,41 @@
 import React from "react"
-import { Helmet } from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
+import {
+    SwipeableDrawer,
+    IconButton,
+    Typography,
+    useMediaQuery,
+    List,
+    Divider,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
 
-export default () => null
+} from "@material-ui/core"
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles"
+import CustomMUI from "../styles/theme"
+
+const SideDrawerStyleSheet = makeStyles(InjectElem => ({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+}))
+
+// ! PROMISES - CREATE TYPES FOR WHAT DATA TO SHOW.
+export default ({ props, children }) => {
+    const DrawerQuery = useStaticQuery(graph`
+    query handleDrawerSetTypes {
+
+    }
+    `)
+    return (
+        <ThemeProvider theme={CustomMUI}>
+            <SwipeableDrawer
+            >
+            </SwipeableDrawer>
+        </ThemeProvider>
+    )
+}
