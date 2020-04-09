eb0c7345968703b8c9218c0a359b288da32c3350
Janrey Licas <janjan.licas@gmail.com>
2020-04-09 14:14:01 +0800

    Gatsby-ConfigJS | Added Socials, crossOrigin and Localization

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
         title_secondary: `I.T.W`,
         description: `Kickstarted React Application that comes with Blogging, Written Works and Portfolio in mind.`,
         author: `CodexLink`,
+        author_name: `Janrey Licas`,
+        socials: {
+            facebook: `facebookCodexLink`,
+            instagram: `instagram__self.codexlink/`,
+            twitter: `twitterCodexLink`,
+            github: `githubCodexLink/`,
+            steam: `steamcommunityid/PrismaticLunatic`,
+        },
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -13,13 +21,24 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `written-works`,
-                short_name: `WrittenWorks`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                name: `inst-typed-works`,
+                short_name: `Instantiated Typed Works`,
+                description: `A Web-App That Contains Both My Portfolio, Projects and Blogging. Both Cherrished and Instantiated Typed Works of Mine. Powered by ReactJS, GatsbyJS, Material-UI and more!`,
+                start_url: `/en/`,
+                lang: `en`,
+                background_color: `#ffc107`,
+                theme_color: `#ffc107`,
                 display: `minimal-ui`,
                 icon: `src/resources/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                crossOrigin: `use-credentials`,
+                localize: [
+                    {
+                        start_url: `/ph/`,
+                        name: `inst-typed-works`,
+                        short_name: `Instantiated Typed Works`,
+                        description: `Ang isang uring-web app na naglalaman ng portpolyo, proyekto at mga blogs. Mga nilalaman ay itinatangi at nabuo sa paaran ng pag type. Ito'y binubuhay gamit ang ReactJS, GatsbyJS, Material-UI at iba pa!`
+                    },
+                ],
             },
         },
         {
